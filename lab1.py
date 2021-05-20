810784987d5b13883d98879084083ef90bcae9f6
GOLUGURI NAGA BALAJI REDDY <84330632+Balajireddy2002@users.noreply.github.com>
2021-05-20 11:51:24 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,23 @@
+a=int(input("enter value"))
+b=int(input("enter value"))
+c=int(input("enter value"))
+sum=a+b+c
+print(sum)
+avg=sum/3
+print(avg)
+
+#expected output:
+#enter value1
+#enter value1
+#enter value1
+#3
+#1.0
+
+#output:
+#enter value1
+#enter value1
+#enter value1
+#3
+#1.0
+
+
