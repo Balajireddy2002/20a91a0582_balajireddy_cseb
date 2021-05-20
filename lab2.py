c4c7edd8ab5345df58b0aa4a1797e93028e067bc
GOLUGURI NAGA BALAJI REDDY <84330632+Balajireddy2002@users.noreply.github.com>
2021-05-20 12:30:44 +0530

    Add files via upload
    indentation

---
+++ lab2.py
@@ -0,0 +1,21 @@
+a=int(input("enter value"))
+b=int(input("enter value"))
+print(a+b)
+if a<b :
+     print("a is small")
+     print("hloooo.....")
+else :
+    print("a is big")
+print("indentation")
+#expected output:
+#enter value5
+#enter value2
+#7
+#a is big
+#indentation
+# output:
+#enter value5
+#enter value2
+#7
+#a is big
+#indentation
