e632795816bda6893c7b9151aced1e2cba47d526
GOLUGURI NAGA BALAJI REDDY <84330632+Balajireddy2002@users.noreply.github.com>
2021-05-21 10:31:50 +0530

    Add files via upload

---
+++ simple if.py
@@ -0,0 +1,16 @@
+#simple if
+a=int(input("enter a value"))
+print(a)
+if(a<10):
+      print("a is small")
+      print("hlo.....")
+ #expected output:     
+'''enter a value1
+1
+a is small
+hlo.....'''
+ #output:     
+'''enter a value1
+1
+a is small
+hlo.....'''
