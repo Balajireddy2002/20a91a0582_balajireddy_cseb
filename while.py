50152200cd3e59cf29cac34b4c6f8772d6095cd4
GOLUGURI NAGA BALAJI REDDY <84330632+Balajireddy2002@users.noreply.github.com>
2021-05-27 12:34:40 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,7 @@
+end_value=int(input("enter end_value value"))
+step_count=int(input("step count"))
+start=int(input("start"))
+while(start<=end_value):
+    print(start)
+    start+=step_count
+print("loop terminated at{}".format(start))
